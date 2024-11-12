be7be5abcd41625525d7f06ae40f9614ffeda059
dieldev <sidiel.dev@gmail.com>
2024-11-12 09:49:41 -0300

    Inicialização do projeto

---
+++ src/test/java/com/api/odontocloud/OdontoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.odontocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
